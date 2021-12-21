e9d81e5b448f9ba0d9f45939e3503c3e5430a18a
davidcoats <david@getsafetycone.com>
2021-12-21 10:27:00 -0800

    Initial commit of source-stub.

---
+++ source/R5T.T0111.X0001.Construction/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0111.X0001.Construction
+{
+    /// <summary>
+    /// Construction project for Library generator related extension method base extensions..
+    /// </summary>
+    public static class Documentation
+    {
+    }
+}
